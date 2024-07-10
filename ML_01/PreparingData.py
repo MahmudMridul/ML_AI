e4974b5e5e758dbaac60bfd7a3b5240f0f518138
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-07-10 09:49:09 +0600

    Add KNN

--- ML_01/PreparingData.py
+++ ML_01/PreparingData.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plot
 from sklearn.preprocessing import StandardScaler
 from imblearn.over_sampling import RandomOverSampler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report
 
 pd.set_option("display.max_rows", None)
 pd.set_option("display.max_columns", None)
@@ -31,20 +33,21 @@ df["class"] = (df["class"] == "g").astype(int)
 
 train, valid, test = np.split(df.sample(frac=1), [int(0.6 * len(df)), int(0.8 * len(df))])
 
-print((train["class"] == 1).sum())
-print((train["class"] == 0).sum())
+# print((train["class"] == 1).sum())
+# print((train["class"] == 0).sum())
 
 
 def scale_dataset(dataframe, oversampling):
     x = dataframe[dataframe.columns[:-1]].values
     y = dataframe[dataframe.columns[-1]].values
 
-    if oversampling:
-        sampler = RandomOverSampler()
-        x, y = sampler.fit(x, y)
-
     scaler = StandardScaler()
     x = scaler.fit_transform(x)
+
+    if oversampling:
+        sampler = RandomOverSampler()
+        x, y = sampler.fit_resample(x, y)
+
     y_reshaped = np.reshape(y, (-1, 1))
 
     data = np.hstack((x, y_reshaped))
@@ -54,3 +57,12 @@ def scale_dataset(dataframe, oversampling):
 train, x_train, y_train = scale_dataset(train, oversampling=True)
 valid, x_valid, y_valid = scale_dataset(valid, oversampling=False)
 test, x_test, y_test = scale_dataset(test, oversampling=False)
+
+# KNN
+
+knn_model = KNeighborsClassifier(n_neighbors=5)
+knn_model.fit(x_train, y_train)
+
+y_pred = knn_model.predict(x_test)
+
+print(classification_report(y_test, y_pred))

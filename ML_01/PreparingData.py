6dbc0623f4a8417952f1ecf0a28b86855ec13cdd
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-07-12 16:23:05 +0600

    Add neural network

--- ML_01/PreparingData.py
+++ ML_01/PreparingData.py
@@ -8,6 +8,7 @@ from sklearn.metrics import classification_report
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
+import tensorflow as tf
 
 pd.set_option("display.max_rows", None)
 pd.set_option("display.max_columns", None)
@@ -36,6 +37,7 @@ df["class"] = (df["class"] == "g").astype(int)
 
 train, valid, test = np.split(df.sample(frac=1), [int(0.6 * len(df)), int(0.8 * len(df))])
 
+
 # print((train["class"] == 1).sum())
 # print((train["class"] == 0).sum())
 
@@ -61,6 +63,7 @@ train, x_train, y_train = scale_dataset(train, oversampling=True)
 valid, x_valid, y_valid = scale_dataset(valid, oversampling=False)
 test, x_test, y_test = scale_dataset(test, oversampling=False)
 
+
 # KNN
 
 # knn_model = KNeighborsClassifier(n_neighbors=5)
@@ -84,7 +87,64 @@ test, x_test, y_test = scale_dataset(test, oversampling=False)
 # print(classification_report(y_test, y_pred))
 
 # Support Vector Machine
-sv_model = SVC()
-sv_model.fit(x_train, y_train)
-y_pred = sv_model.predict((x_test))
-print(classification_report(y_test, y_pred))
+# sv_model = SVC()
+# sv_model.fit(x_train, y_train)
+# y_pred = sv_model.predict((x_test))
+# print(classification_report(y_test, y_pred))
+
+# Neural Network
+
+def plot_history(history):
+    fig, (ax1, ax2) = plot.subplots(1, 2, figsize=(10, 4))
+    ax1.plot(history.history['loss'], label='loss')
+    ax1.plot(history.history['val_loss'], label='val_loss')
+    ax1.set_xlabel('Epoch')
+    ax1.set_ylabel('Binary crossentropy')
+    ax1.grid(True)
+
+    ax2.plot(history.history['accuracy'], label='accuracy')
+    ax2.plot(history.history['val_accuracy'], label='val_accuracy')
+    ax2.set_xlabel('Epoch')
+    ax2.set_ylabel('Accuracy')
+    ax2.grid(True)
+
+    plot.show()
+
+
+def train_model(X_train, y_train, num_nodes, dropout_prob, lr, batch_size, epochs):
+    nn_model = tf.keras.Sequential([
+        tf.keras.layers.Dense(num_nodes, activation='relu'),
+        tf.keras.layers.Dropout(dropout_prob),
+        tf.keras.layers.Dense(num_nodes, activation='relu'),
+        tf.keras.layers.Dropout(dropout_prob),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
+
+    nn_model.compile(optimizer=tf.keras.optimizers.Adam(lr), loss='binary_crossentropy',
+                     metrics=['accuracy'])
+    history = nn_model.fit(
+        X_train, y_train, epochs=epochs, batch_size=batch_size, validation_split=0.2, verbose=0
+    )
+
+    return nn_model, history
+
+
+least_val_loss = float('inf')
+least_loss_model = None
+epochs = 100
+for num_nodes in [16, 32, 64]:
+    for dropout_prob in [0, 0.2]:
+        for lr in [0.01, 0.005, 0.001]:
+            for batch_size in [32, 64, 128]:
+                print(f"{num_nodes} nodes, dropout {dropout_prob}, lr {lr}, batch size {batch_size}")
+                model, history = train_model(x_train, y_train, num_nodes, dropout_prob, lr, batch_size, epochs)
+                plot_history(history)
+                val_loss = model.evaluate(x_valid, y_valid)[0]
+                if val_loss < least_val_loss:
+                    least_val_loss = val_loss
+                    least_loss_model = model
+
+
+y_pred = least_loss_model.predict(x_test)
+y_pred = (y_pred > 0.5).astype(int).reshape(-1,)
+

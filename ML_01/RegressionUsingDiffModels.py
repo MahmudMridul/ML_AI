703cdd72d9ce7c27722558dc54a1685220af23fe
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-07-16 13:40:12 +0600

    Linear regression

--- ML_01/RegressionUsingDiffModels.py
+++ ML_01/RegressionUsingDiffModels.py
@@ -1,22 +1,63 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from imblearn.over_sampling import RandomOverSampler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report
 from sklearn.naive_bayes import GaussianNB
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression
 from sklearn.svm import SVC
 # import tensorflow as tf
+import copy
 
 pd.set_option("display.max_rows", None)
 pd.set_option("display.max_columns", None)
 pd.set_option("display.width", None)
 
-dataset_cols = ["bike_count", "hour", "temp", "humidity", "wind", "visibility", "dew_pt_temp", "radiation", "rain", "snow", "functional"]
+dataset_cols = ["bike_count", "hour", "temp", "humidity", "wind", "visibility", "dew_pt_temp", "radiation", "rain",
+                "snow", "functional"]
 df = pd.read_csv("../Datasets/SeoulBikeData.csv").drop(["Date", "Holiday", "Seasons"], axis=1)
 df.columns = dataset_cols
 df["functional"] = (df["functional"] == "Yes").astype(int)
 
-print(df.head(10))
+df = df[df["hour"] == 12]
+df = df.drop(["hour"], axis=1)
+
+# for label in df.columns[1:]:
+#     plt.scatter(df[label], df["bike_count"])
+#     plt.title(label)
+#     plt.ylabel("Bike Count at Noon")
+#     plt.xlabel(label)
+#     plt.show()
+
+df = df.drop(["wind", "visibility", "functional"], axis=1)
+
+train, val, test = np.split(df.sample(frac=1), [int(0.6 * len(df)), int(0.8 * len(df))])
+
+
+def get_xy(dataframe, y_label, x_labels=None):
+    dataframe = copy.deepcopy(dataframe)
+    if x_labels is None:
+        X = dataframe[[c for c in dataframe.columns if c != y_label]].values
+    else:
+        if len(x_labels) == 1:
+            X = dataframe[x_labels[0]].values.reshape(-1, 1)
+        else:
+            X = dataframe[x_labels].values
+
+    y = dataframe[y_label].values.reshape(-1, 1)
+    data = np.hstack((X, y))
+
+    return data, X, y
+
+
+_, X_train_temp, y_train_temp = get_xy(train, "bike_count", x_labels=["temp"])
+_, X_val_temp, y_val_temp = get_xy(val, "bike_count", x_labels=["temp"])
+_, X_test_temp, y_test_temp = get_xy(test, "bike_count", x_labels=["temp"])
+
+temp_reg = LinearRegression()
+temp_reg.fit(X_train_temp, y_train_temp)
+
+temp_reg.score(X_test_temp, y_test_temp)
+

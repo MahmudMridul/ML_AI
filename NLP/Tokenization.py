8c10ce4c7f43a4506cd1c5f036454bf2b99e1be3
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-09-15 15:58:19 +0600

    Modify get_corpus function

--- NLP/Tokenization.py
+++ NLP/Tokenization.py
@@ -5,8 +5,8 @@ from nltk.tokenize import TreebankWordTokenizer
 
 file_path = '../Datasets/NLP/chatgpt-generated.txt'
 
-def get_corpus():
-    with open(file_path, 'r') as file:
+def get_corpus(f_path):
+    with open(f_path, 'r') as file:
         corps = file.read()
     return corps
 
@@ -26,7 +26,7 @@ def get_words_from_corpus(corp, tokenizer = 0):
 
 
 if __name__ == '__main__':
-    corpus = get_corpus()
+    corpus = get_corpus(file_path)
     documents = get_documents(corpus)
     words = get_words_from_corpus(corpus)
     print(words)
